57a32beff5c8452fedc12530ea0023f8caafbec8
MarkLeahy2 <82410431+MarkLeahy2@users.noreply.github.com>
2022-04-22 20:22:23 +0100

    Main menu added to Main.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,20 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import java.util.*
 
-    // Try adding program arguments at Run/Debug configuration
-    println("Program arguments: ${args.joinToString()}")
+val scanner = Scanner(System.`in`)
+
+fun mainMenu() : Int {
+    println("")
+    println("--------------------")
+    println("NOTE KEEPER APP")
+    println("--------------------")
+    println("NOTE MENU")
+    println("  1) Add a note")
+    println("  2) List all notes")
+    println("  3) Update a note")
+    println("  4) Delete a note")
+    println("--------------------")
+    println("  0) Exit")
+    println("--------------------")
+    print("==>> ")
+    return scanner.nextInt()
 }
