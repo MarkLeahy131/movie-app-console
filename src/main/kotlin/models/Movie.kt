c462d1eaa6c3c281c6a273deb8b31b363da819b9
MarkLeahy2 <82410431+MarkLeahy2@users.noreply.github.com>
2022-04-28 10:55:15 +0100

    collection-and-model created

---
+++ src/main/kotlin/models/Movie.kt
@@ -0,0 +1,7 @@
+package models
+
+data class Note(val movieGenre: String,
+                val movieDirector: String,
+                val movieDuration: Float,
+                val movieRating :Double){
+}
